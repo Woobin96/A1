290a109101a906ae4a8ca7f12d54ac22557ff7b8
woobin <hmy1219@naver.com>
2021-02-08 10:11:57 +0900

    11

--- app/src/main/java/com/wooeun18/a1/MainActivity.java
+++ app/src/main/java/com/wooeun18/a1/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //asdasdasdasd
+
+        getSupportActionBar().setTitle("aaaa");
     }
 }
